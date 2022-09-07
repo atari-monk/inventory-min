607c19ed13db0606a4b5d69c183c3608aeb6502c
krzm <kmazanek@gmail.com>
2022-09-07 20:58:43 +0200

    add 6 props to item

--- Inventory.Min.Mvc.Web.App/Models/ItemVM.cs
+++ Inventory.Min.Mvc.Web.App/Models/ItemVM.cs
@@ -1,10 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Inventory.Min.Data;
+using ModelHelper;
+
 namespace Inventory.Min.Mvc.Web.App.Models;
 
 public class ItemVM
+    : Model
 {
     public int Id { get; set; }
 
+    [Required
+        , MaxLength(NameMax)]
     public string? Name { get; set; }
 
+    [MaxLength(DescriptionMax)]
     public string? Description { get; set; }
+
+    public int? InitialCount { get; set; }
+
+    public int? CurrentCount { get; set; }
+
+    [ForeignKey(nameof(Category))]
+	public int? CategoryId { get; set; }
+
+	[Column(TypeName = Datetime2Name)]
+	public DateTime? PurchaseDate { get; set; }
+
+    [ForeignKey(nameof(Currency))]
+	public int? CurrencyId { get; set; }
+
+    [Column(TypeName = "decimal(5, 2)")]
+    public decimal? PurchasePrice { get; set; }
 }
